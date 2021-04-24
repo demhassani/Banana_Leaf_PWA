9c5e2ec343380d14442d3d60e0efcf7f7e781777
Hassani <demhassani@gmail.com>
2021-04-24 16:59:54 +0430

    add static and dynamic caching

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,50 @@
-console.log("ff")
+let CACHE_VERSION = 1.0;
+
+let CURRENT_CACHE = {
+  static: "static-cache-v" + CACHE_VERSION,
+  dynamic: "dynamic-cache-v" + CACHE_VERSION,
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CURRENT_CACHE["static"]).then((cache) => {
+      cache.addAll([
+        "/",
+        "/public/css/templatemo-style.css",
+        "/public/js/app.js",
+        "/public/js/jquery.min.js",
+        "/public/js/parallax.min.js",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  let expectedCacheNames = Object.values(CURRENT_CACHE);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.forEach((cacheName) => {
+          if (!expectedCacheNames.includes(cacheName))
+            return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+
+      return fetch(event.request).then((networkResponse) => {
+        caches.open(CURRENT_CACHE["dynamic"]).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
